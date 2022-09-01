7aa12c457f554dae5a0b57809c4a51058890c81c
Gustavo Laurindo dos Santos <104037685+lawrindovsk@users.noreply.github.com>
2022-08-31 21:08:08 -0300

    Update 1.0

--- Calc.py
+++ Calc.py
@@ -5,37 +5,38 @@ class Calc:
         pass
 
     def somar(self, num1, num2):
-        return float(num1 + num2)
+        return float(num1) + float(num2)
 
     def subtrair(self, num1, num2):
-        return float(num1 - num2)
+        return float(num1) - float(num2)
 
     def multiplicar(self, num1, num2):
-        return float(num1 * num2)
+        return float(num1) * float(num2)
 
     def dividir(self, num1, num2):
         if float(num2) == 0:
             return "Impossível dividir por zero!"
         else:
-            return float(num1/num2)
+            return float(num1)/float(num2)
 
     def potencia(self, base, expoente):
-        if expoente == 0:
+        if float(expoente) == 0:
             return 1
-        elif expoente == 1:
-            return base
+        elif float(expoente) == 1:
+            return float(base)
         else:
-            return math.pow(float(base, expoente))
+            return math.pow(float(base), float(expoente))
 
     def raiz(self, num):
         if float(num) < 0:
             return "Impossível calcular"
         else:
-            return math.sqr(float(num)) 
+            return math.sqrt(float(num))
 
     def tabuada(self, num):
+        resultado = ""
         for i in range(11):
-            resultado = resultado + "\n{} * {} = {}".format(num, i, float(float(num*i)))
+            resultado = resultado + "\n{} * {} = {}".format(num, i, float(float(num)*float(i)))
         return resultado
 
 
--- main.py
+++ main.py
@@ -5,82 +5,74 @@ import this
 
 calculadora = Calc()
 app = Flask(__name__)#criando uma variável APP que guarda os elementos do Flask
-this.resultadoFinal =""
+this.resultadoFinal = ""
 
-@app.route("/")#Página Index - Primeira página de qualquer website
+@app.route("/")#Página Index - Primeira Página de Qualquer Site
 def homepage():
-    return render_template("index.html")
+    return render_template("Index.html")
 
-##########
 @app.route("/soma", methods=['POST','GET'])
 def soma():
-    if request.method == 'POST':
-        num1 = request.form['num1']
-        num2 = request.form['num2']
-        this.resultadoFinal= calculadora.somar(num1,num2)
-    return render_template("soma.html", titulo="Soma", resultado=this.resultadoFinal)
-
-#########       
-@app.route("/subtracao", methods=['POST','GET'])
-def subt():
-    if request.methode == 'POST':
-        numero1 = request.form['num1'] 
-        numero2 = request.form['num2'] 
-        this.resultadoFinal = calculadora.subtrair(numero1,numero2)
-        return render_template("subtracao.html",titulo='Subtrair',resultado=this.resultadoFinal)
-
-########
-@app.route("/divisao", methods=['POST',"GET"])
-def divi():
-
     this.resultado = 0
     if request.method == 'POST':
         numero1 = request.form['num1']
-        numero2 = request.form['num2'] #mudar aqui
-        this.resultadoFinal = calculadora.dividir(numero1, numero2)
-        return render_template("divisao.html",titulo='Dividir',resultado=this.resultadoFinal)
-
-#######
-@app.route("/multiplicacao", methods=['POST',"GET"])
-def mult():
-    if request.method == 'POST':
-        numero1 = request.form['num1']
-        numero2 = request.form['num2'] #mudar aqui
-        this.resultadoFinal = calculadora.dividir(numero1, numero2)
-        return render_template("multiplicacao.html",titulo='Multiplicação',resultado=this.resultadoFinal)
-
-########
-@app.route("/potencia", methods=['POST',"GET"])
-def pot()):
-    if request.method == 'POST':
-        numero1 = request.form['num1']
-        numero2 = request.form['num2'] 
-        this.resultadoFinal = calculadora.potencia(numero1, numero2)
-        return render_template("potencia.html",titulo='Potência',resultado=this.resultadoFinal)
-
-#########
-@app.route("/raiz", methods=['POST',"GET"])
-def raiz():
-    if request.method == 'POST':
-        numero1 = request.form['num1']
-        numero2 = request.form['num2'] 
-        this.resultadoFinal = calculadora.potencia(numero1, numero2)
-        return render_template("raiz.html",titulo='Raíz',resultado=this.resultadoFinal)
-
-#########
-@app.route("/tabuada", methods=['POST','GET'])
-def tab():
+        numero2 = request.form['num2']
+        this.resultadoFinal = calculadora.somar(numero1,numero2)
+    return render_template("soma.html", titulo="Soma", resultado=this.resultadoFinal)
+        
+@app.route("/subtracao", methods=['POST','GET'])
+def subt():
+    this.resultado = 0   
     if request.method == 'POST':
         numero1 = request.form['num1']
         numero2 = request.form['num2']
-        this.resultadoFinal = calculadora.tabuada(numero1, numero2)
-        return render_template("tabuada.html",título='Raíz',resultado=this.resultadoFinal)
+        this.resultadoFinal = calculadora.subtrair(numero1, numero2)
+    return render_template("subtracao.html",titulo='Subtrair',resultado=this.resultadoFinal)        
+
+@app.route("/divisao", methods=['POST', 'GET'])
+def divi():
+    this.resultado = 0
+    if request.method == 'POST':
+        numero1 = request.form['num1']
+        numero2 = request.form['num2']
+        this.resultadoFinal = calculadora.dividir(numero1, numero2)
+    return render_template("divisao.html", titulo='Dividir', resultado=this.resultadoFinal)
+
+@app.route('/multiplicacao', methods=['POST', 'GET'])
+def mult():
+    this.resultado = 0
+    if request.method == 'POST':
+        numero1 = request.form['num1']
+        numero2 = request.form['num2']
+        this.resultadoFinal = calculadora.multiplicar(numero1, numero2)
+    return render_template("multiplicacao.html", titulo='Multiplicar', resultado=this.resultadoFinal)
+
+@app.route('/potencia', methods=['POST', 'GET'])
+def pote():
+    this.resultado = 0
+    if request.method == 'POST':
+        numero1 = request.form['num1']
+        numero2 = request.form['num2']
+        this.resultadoFinal = calculadora.potencia(numero1, numero2)
+    return render_template("potencia.html", titulo='Potência', resultado=this.resultadoFinal)
+
+@app.route('/raiz', methods=['POST', 'GET'])
+def raizes():
+    this.resultado = 0
+    if request.method == 'POST':
+        numero1 = request.form['num1']
+        this.resultadoFinal = calculadora.raiz(numero1)
+    return render_template("raiz.html", titulo='Raiz', resultado=this.resultadoFinal)
+
+@app.route('/tabuada', methods=['POST', 'GET'])
+def tabuad():
+    this.resultado = 0
+    if request.method == 'POST':
+        numero1 = request.form['num1']
+        this.resultadoFinal = calculadora.tabuada(numero1)
+    return render_template("tabuada.html", titulo='Tabuada', resultado=this.resultadoFinal)
 
 
-
-
-
-# Run the script:
+# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     app.run(debug=True)
-
